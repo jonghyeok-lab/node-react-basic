ae6c075ac1068ba83b198e22ba7f2e2e9a7925a8
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-08-01 08:54:50 +0900

    끝말잇기

--- webpack/WordRelay.jsx
+++ webpack/WordRelay.jsx
@@ -1,12 +1,40 @@
 let React = require('react');
+const {useState, useRef} = require("react");
 
-class WordRelay extends React.Component {
-    state = {
+const WordRelay = () => {
+    const [word, setWord] = useState('제로초 ');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
 
+    const inputRef = useRef(null);
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if(word[word.length - 1] === value[0]) {
+            setResult('딩동댕');
+            setWord(value);
+            setValue('');
+        } else {
+            setResult('땡!')
+            setValue('')
+        }
+        inputRef.current.focus();
     }
 
-    render() {
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
     }
-}
+
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input ref = {inputRef} value={value} onChange={onChangeInput} />
+                <button>클릭!</button>
+            </form>
+            <div>{result}</div>
+        </>
+    );
+};
 
 module.exports = WordRelay;
