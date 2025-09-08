956c47285f8f9447b0c00f46157502e6347c2f4e
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-09-08 23:52:20 +0900

    생활코딩 리액트 - 이벤트

--- egoing-react/src/App.tsx
+++ egoing-react/src/App.tsx
@@ -3,51 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 interface NavProps {
-  id: number,
-  title: string,
-  body: string
+    id: number,
+    title: string,
+    body: string
 }
 
 interface NavListProps {
-  props: NavProps[]
+    props: NavProps[],
+    onClick: () => void
 }
 
-const Nav = ({props}: NavListProps) => {
-  const li = props.map(prop => <li key={prop.id}><a href={"/read/" + prop.id}>{prop.title}</a></li>);
+const Nav = ({props, onClick}: NavListProps) => {
+    const li = props.map(prop => <li key={prop.id}><a href={"/read/" + prop.id}>{prop.title}</a></li>);
 
-  return (
-      <ul>
-        {li}
-      </ul>
-  )
+    return (
+        <>
+            <a href="#" onChange={onClick}>
+                Do Click!
+            </a>
+            <ul>
+                {li}
+            </ul>
+        </>
+    )
 }
 
 function App() {
-  const props:NavProps[] = [
-    {id: 1, title: "hello", body: "hello body"},
-    {id: 2, title: "hello2", body: "hello2 body"},
-    {id: 3, title: "hello3", body: "hello3 body"}
-  ]
+    const props: NavProps[] = [
+        {id: 1, title: "hello", body: "hello body"},
+        {id: 2, title: "hello2", body: "hello2 body"},
+        {id: 3, title: "hello3", body: "hello3 body"}
+    ]
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Nav props={props} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <Nav props={props} onClick={function () {
+                alert('click!!!!')
+            }}/>
+        </div>
+    );
 }
 
 export default App;
